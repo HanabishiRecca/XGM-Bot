66b63c826e4f4c6165500014d655454951e4ca6e
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2019-09-10 19:13:02 +0500

    Warn color

--- log.js
+++ log.js
@@ -6,7 +6,7 @@ const ConsoleLog = console.log;
 console.log = (message, ...params) => ConsoleLog(`[${CurrentDT()}] ${message}`, ...params);
 
 const ConsoleWarn = console.warn;
-console.warn = (message, ...params) => ConsoleWarn(`\x1b[33m[${CurrentDT()}] ${message}\x1b[0m`, ...params);
+console.warn = (message, ...params) => ConsoleWarn(`\x1b[93m[${CurrentDT()}] ${message}\x1b[0m`, ...params);
 
 const ConsoleError = console.error;
 console.error = (message, ...params) => ConsoleError(`\x1b[31m[${CurrentDT()}] ${message}\x1b[0m`, ...params);
