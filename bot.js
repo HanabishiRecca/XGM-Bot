161d12fb9438dd796bece70307d4db88a2063016
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2020-03-27 19:58:14 +0500

    Changed format and newline

--- bot.js
+++ bot.js
@@ -73,13 +73,13 @@ const HasPermission = (server, member, flag) => {
     const
         serverRoles = server.roles,
         roles = member.roles;
-    
+
     for(let i = 0; i < roles.length; i++) {
         const role = serverRoles.get(roles[i]);
         if(role && CheckPermission(role.permissions, flag))
             return true;
     }
-    
+
     return false;
 };
 
@@ -105,61 +105,61 @@ const botCommands = {
 
 *Команды можно отправлять боту в ЛС.*`);
     },
-    
+
     whois: async message => {
         if(!(message.mentions && message.mentions.length))
             return;
-        
+
         const userData = await usersDb.findOne({ _id: message.mentions[0] });
         message.reply(userData ? `https://xgm.guru/user/${userData.xgmid}` : 'Пользователь не сопоставлен.');
     },
-    
+
     warn: async message => {
         if(!(message.mentions && message.mentions.length))
             return;
-        
+
         if(!HasPermission(message.server, message.member, Permissions.MANAGE_MESSAGES))
             return;
-        
+
         const userId = message.mentions[0];
         if(userId == client.user.id)
             return;
-        
+
         const user = await SafePromise(GetUser(userId));
         if(!user)
             return message.reply('Указанный пользователь не существует.');
-        
+
         const member = message.server.members.get(user.id);
         if(member && HasPermission(message.server, member, Permissions.MANAGE_MESSAGES))
             return;
-        
+
         const
             warnState = await warnsDb.findOne({ _id: user.id }),
             warns = warnState ? (warnState.warns + 1) : 1;
-        
+
         if(warns >= config.maxWarns)
             SafePromise(AddRole(message.server, user, config.role.readonly));
-        
+
         await warnsDb.update({ _id: user.id }, { $set: { warns: warns, dt: Date.now() } }, { upsert: true });
-        
+
         SendMessage(config.channel.log, `Пользователь ${UserMention(user)} получил предупреждение ${warns}/${config.maxWarns}!\nВыдано модератором ${UserMention(message.author)}`);
         SendPM(user, `Вы получили предупреждение ${warns}/${config.maxWarns}!`);
     },
-    
+
     list: async message => {
         if(!HasPermission(message.server, message.member, Permissions.MANAGE_MESSAGES))
             return;
-        
+
         const
             warnStates = await warnsDb.find({}),
             now = Date.now();
-        
+
         let text = `**Нарушителей:** ${warnStates.length}\n\n`;
         for(let i = 0; i < warnStates.length; i++) {
             const
                 warnState = warnStates[i],
                 add = `${UserMention(warnState._id)} → ${FormatWarn(warnState, now)}\n`;
-            
+
             if(text.length + add.length < 2000) {
                 text += add;
             } else {
@@ -167,10 +167,10 @@ const botCommands = {
                 text = add;
             }
         }
-        
+
         SendMessage(message.channel_id, text);
     },
-    
+
     status: async message => {
         const warnState = await warnsDb.findOne({ _id: message.author.id });
         message.reply(warnState ? FormatWarn(warnState, Date.now()) : 'Нет предупреждений.');
@@ -181,20 +181,20 @@ const WarnTick = async () => {
     const warnStates = await warnsDb.find({});
     if(warnStates.length < 1)
         return;
-    
+
     const now = Date.now();
     for(let i = 0; i < warnStates.length; i++) {
         const warnState = warnStates[i];
         if((warnState.dt + warnPeriod) > now)
             continue;
-        
+
         warnState.warns--;
-        
+
         if(warnState.warns > 0)
             await warnsDb.update({ _id: warnState._id }, { $set: { warns: warnState.warns, dt: warnState.dt + warnPeriod } });
         else
             await warnsDb.remove({ _id: warnState._id });
-        
+
         if(warnState.warns < config.maxWarns)
             SafePromise(RemoveRole(config.server, warnState._id, config.role.readonly));
     }
@@ -206,7 +206,7 @@ const MarkMessages = (() => {
     const
         list = require('./marks.js').list,
         msgs = new Map();
-    
+
     for(let i = 0; i < list.length; i++) {
         const mark = list[i];
         if(msgs.has(mark.message))
@@ -214,7 +214,7 @@ const MarkMessages = (() => {
         else
             msgs.set(mark.message, { id: mark.message, channel: mark.channel, marks: [mark] });
     }
-    
+
     return msgs;
 })();
 
@@ -222,7 +222,7 @@ const ReactionProc = async (reaction, add) => {
     const msg = MarkMessages.get(reaction.message_id);
     if(!msg)
         return;
-    
+
     const mark = msg.marks.find(elem => elem.emoji == reaction.emoji.id);
     if(mark)
         (add ? AddRole : RemoveRole)(reaction.guild_id, reaction.user_id, mark.role);
@@ -231,25 +231,25 @@ const ReactionProc = async (reaction, add) => {
 const SetMarks = async serverEmojis => {
     if(MarkMessages.synced)
         return;
-    
+
     MarkMessages.synced = true;
-    
+
     const emojiMap = new Map();
     for(let i = 0; i < serverEmojis.length; i++) {
         const emoji = serverEmojis[i];
         emojiMap.set(emoji.id, emoji);
     }
-    
+
     for(const msg of MarkMessages.values()) {
         const message = await GetMessage(msg.channel, msg.id);
         if(!message)
             continue;
-        
+
         for(let i = 0; i < msg.marks.length; i++) {
             const mark = msg.marks[i];
             if(message.reactions.find(elem => elem.emoji.id == mark.emoji))
                 continue;
-            
+
             const emoji = emojiMap.get(mark.emoji);
             await AddReaction(message.channel_id, message.id, `${emoji.name}:${emoji.id}`);
         }
@@ -264,26 +264,26 @@ const CheckNews = async () => {
     const data = await Misc.HttpsGet('https://xgm.guru/rss');
     if(!(data && data.length))
         return;
-    
+
     const feed = XmlParser.parse(Misc.Win1251ToUtf8(data), { ignoreAttributes: false, attributeNamePrefix: '' });
     if(!(feed.rss && feed.rss.channel && feed.rss.channel.item))
         return;
-    
+
     const
         option = await appDb.findOne(appOptions.lastNewsTime),
         lastTime = option ? option.value : Date.now(),
         items = feed.rss.channel.item;
-    
+
     let maxTime = 0;
     for(let i = items.length - 1; i >= 0; i--) {
         const
             item = items[i],
             dt = new Date(item.pubDate),
             time = dt.getTime();
-        
+
         if(time > maxTime)
             maxTime = time;
-        
+
         if(time > lastTime) {
             const embed = {
                 title: Misc.DecodeHtmlEntity(item.title),
@@ -295,12 +295,12 @@ const CheckNews = async () => {
                 image: item.enclosure ? { url: item.enclosure.url } : null,
             };
             SendMessage(config.channel.news, '', embed);
-            
+
             embed.timestamp = undefined;
             SendMessage(config.channel.newsCode, `\`\`\`b/post\n${JSON.stringify({ content: 'https://discord.gg/TuSHPU6', embed }, null, 4)}\`\`\``);
         }
     }
-    
+
     if(lastTime != maxTime)
         await appDb.update(appOptions.lastNewsTime, { $set: { value: maxTime } }, { upsert: true });
 };
@@ -311,7 +311,7 @@ const CheckTwilight = async (server, member, xgmid) => {
     const response = JSON.parse(await Misc.HttpsGet(`https://xgm.guru/api_user.php?id=${xgmid}`));
     if(!response)
         return;
-    
+
     if(response.info.user.seeTwilight) {
         if(member.roles.indexOf(config.role.twilight) < 0)
             await AddRole(server, member.user, config.role.twilight);
@@ -325,12 +325,12 @@ const SyncTwilight = async () => {
     const
         server = ConnectedServers.get(config.server),
         users = await usersDb.find({});
-    
+
     for(let i = 0; i < users.length; i++) {
         const
             userInfo = users[i],
             member = server.members.get(userInfo._id);
-        
+
         if(member)
             await CheckTwilight(server, member, userInfo.xgmid);
     }
@@ -341,10 +341,10 @@ setInterval(SyncTwilight, 3600000);
 const SaveMessage = async message => {
     if(!mdbConnectionOptions)
         return;
-    
+
     if(!(message.content && message.guild_id))
         return;
-    
+
     const connection = await MariaDB.createConnection(mdbConnectionOptions);
     try {
         await connection.query({ namedPlaceholders: true, sql: 'insert into messages (id,user,text) values (:id,:user,:text) on duplicate key update text=:text;' }, { id: message.id, user: message.author.id, text: message.content });
@@ -358,7 +358,7 @@ const SaveMessage = async message => {
 const LoadMessage = async message => {
     if(!mdbConnectionOptions)
         return;
-    
+
     let results;
     const connection = await MariaDB.createConnection(mdbConnectionOptions);
     try {
@@ -368,7 +368,7 @@ const LoadMessage = async message => {
     } finally {
         connection.end();
     }
-    
+
     if(results && results.length)
         return results[0];
 };
@@ -398,70 +398,70 @@ const events = {
     READY: async data => {
         client.user = data.user;
         client.WsSend({ op: 3, d: { status: { web: 'online' }, game: { name: '/help', type: 3 }, afk: false, since: 0 } });
-        
+
         ConnectedServers.clear();
-        
+
         const servers = data.guilds;
         for(let i = 0; i < servers.length; i++) {
             const server = servers[i];
             ConnectedServers.set(server.id, server);
         }
-        
+
         console.log('READY');
     },
-    
+
     MESSAGE_CREATE: async message => {
         if(message.guild_id && (message.guild_id != config.server))
             return;
-        
+
         if(!message.content)
             return;
-        
+
         if(message.author.id == client.user.id)
             return;
-        
+
         message.guild_id && SaveMessage(message);
-        
+
         if(!message.content.startsWith(config.prefix))
             return;
-        
+
         const
             si = message.content.search(/(\s|\n|$)/),
             command = message.content.substring(config.prefix.length, (si > 0) ? si : undefined).toLowerCase();
-        
+
         if(!(command && botCommands.hasOwnProperty(command)))
             return;
-        
+
         message.content = message.content.substring((si > 0) ? (si + 1) : '');
         message.mentions = Misc.GetMentions(message.content);
         message.reply = content => SendMessage(message.channel_id, message.guild_id ? `${UserMention(message.author)}\n${content}` : content);
-        
+
         message.server = message.guild_id ? ConnectedServers.get(message.guild_id) : ConnectedServers.get(config.server);
-        
+
         if(!message.member)
             message.member = message.server.members.get(message.author.id);
-        
+
         console.log(`COMMAND (${command}) ARG (${message.content}) USER (${message.author.username}#${message.author.discriminator}) ${message.guild_id ? 'SERVER' : 'PM'}`);
         botCommands[command](message);
     },
-    
+
     MESSAGE_UPDATE: async message => {
         if(message.guild_id != config.server)
             return;
-        
+
         if(!message.author)
             return;
-        
+
         if(message.author.id == client.user.id)
             return;
-        
+
         const result = await LoadMessage(message);
-        
+
         SaveMessage(message);
-        
+
         if(!result)
             return;
-        
+
         SendMessage(config.channel.deleted, '', {
             title: 'Сообщение изменено',
             fields: [{
@@ -486,15 +486,15 @@ const events = {
             timestamp: new Date(result.dt),
         });
     },
-    
+
     MESSAGE_DELETE: async message => {
         if(message.guild_id != config.server)
             return;
-        
+
         const result = await LoadMessage(message);
         if(!result)
             return;
-        
+
         SendMessage(config.channel.deleted, '', {
             title: 'Сообщение удалено',
             fields: [{
@@ -515,104 +515,104 @@ const events = {
             timestamp: new Date(result.dt),
         });
     },
-    
+
     GUILD_MEMBER_ADD: async member => {
         const server = ConnectedServers.get(member.guild_id);
         server && server.members.set(member.user.id, member);
-        
+
         if(member.guild_id != config.server)
             return;
-        
+
         SendMessage(config.channel.log, `<:zplus:544205514943365123> ${UserMention(member.user)} присоединился к серверу.`);
-        
+
         const warnState = await warnsDb.findOne({ _id: member.user.id });
         if(warnState && (warnState.warns >= config.maxWarns))
             AddRole(member.guild_id, member.user, config.role.readonly);
-        
+
         const userInfo = await usersDb.findOne({ _id: member.user.id });
         if(userInfo) {
             AddRole(member.guild_id, member.user, config.role.user);
             CheckTwilight(member.guild_id, member, userInfo.xgmid);
         }
     },
-    
+
     GUILD_MEMBER_UPDATE: async member => {
         const server = ConnectedServers.get(member.guild_id);
         server && server.members.set(member.user.id, member);
     },
-    
+
     GUILD_MEMBER_REMOVE: async member => {
         const server = ConnectedServers.get(member.guild_id);
         server && server.members.delete(member.user.id);
-        
+
         if(member.guild_id == config.server)
             SendMessage(config.channel.log, `<:zminus:544205486073839616> ${UserMention(member.user)} покинул сервер.`);
     },
-    
+
     MESSAGE_REACTION_ADD: async reaction => {
         if(reaction.guild_id != config.server)
             return;
-        
+
         if(client.user.id != reaction.user_id)
             ReactionProc(reaction, true);
     },
-    
+
     MESSAGE_REACTION_REMOVE: async reaction => {
         if(reaction.guild_id != config.server)
             return;
-        
+
         if(client.user.id != reaction.user_id)
             ReactionProc(reaction, false);
     },
-    
+
     GUILD_CREATE: async server => {
         AddServer(server);
         client.WsSend({ op: 8, d: { guild_id: server.id, query: '', limit: 0 } });
-        
+
         if(server.id != config.server)
             return;
-        
+
         SetMarks(server.emojis);
         CheckNews();
     },
-    
+
     GUILD_UPDATE: async update => {
         const server = ConnectedServers.get(update.id);
         server && UpdateServer(server, update);
     },
-    
+
     GUILD_DELETE: async deleted => {
         !deleted.unavailable && ConnectedServers.delete(deleted.id);
     },
-    
+
     GUILD_MEMBERS_CHUNK: async chunk => {
         const server = ConnectedServers.get(chunk.guild_id);
         if(!server)
             return;
-        
+
         const
             map = server.members,
             members = chunk.members;
-        
+
         for(let i = 0; i < members.length; i++) {
             const member = members[i];
             map.set(member.user.id, member);
         }
-        
+
         if((server.id == config.server) && (chunk.members.length < 1000))
             SyncTwilight();
     },
-    
+
     GUILD_ROLE_CREATE: async data => {
         const server = ConnectedServers.get(data.guild_id);
         server && server.roles.set(data.role.id, data.role);
     },
-    
+
     GUILD_ROLE_UPDATE: async data => {
         const server = ConnectedServers.get(data.guild_id);
         server && server.roles.set(data.role.id, data.role);
     },
-    
+
     GUILD_ROLE_DELETE: async data => {
         const server = ConnectedServers.get(data.guild_id);
         server && server.roles.delete(data.role_id);
@@ -640,11 +640,11 @@ const VerifyUser = async (code, xgmid) => {
     const res = await SafePromise(client.Request('post', '/oauth2/token', `client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=authorization_code&code=${encodeURIComponent(code)}&redirect_uri=${REDIRECT_URL}&scope=identify`));
     if(!(res && res.access_token))
         return 400;
-    
+
     const user = await SafePromise(client.Request('get', Routes.User('@me'), null, `Bearer ${res.access_token}`));
     if(!(user && user.id))
         return 500;
-    
+
     let retCode;
     if(await usersDb.findOne({ _id: user.id })) {
         SendPM(user, 'Аккаунт уже подтвержден.');
@@ -660,13 +660,13 @@ const VerifyUser = async (code, xgmid) => {
         SendPM(user, ':white_check_mark: Аккаунт подтвержден!');
         retCode = 200;
     }
-    
+
     const member = ConnectedServers.get(config.server).members.get(user.id);
     if(member) {
         AddRole(config.server, user, config.role.user);
         CheckTwilight(config.server, member, xgmid);
     }
-    
+
     return { code: retCode, content: user.id };
 };
 
@@ -676,23 +676,23 @@ require('http').createServer(async (request, response) => {
         response.end();
         return;
     }
-    
+
     if(request.headers.authorization != AUTH_SVC) {
         response.statusCode = 401;
         response.end();
         return;
     }
-    
+
     const
         code = request.headers.code,
         xgmid = Number(request.headers.userid);
-    
+
     if(!(code && (xgmid > 0))) {
         response.statusCode = 400;
         response.end();
         return;
     }
-    
+
     let ret;
     try {
         ret = await VerifyUser(code, xgmid);
@@ -702,7 +702,7 @@ require('http').createServer(async (request, response) => {
         response.end();
         return;
     }
-    
+
     if(ret.code) {
         response.statusCode = ret.code;
         if(ret.content) {
@@ -712,6 +712,6 @@ require('http').createServer(async (request, response) => {
     } else {
         response.statusCode = ret;
     }
-    
+
     response.end();
 }).listen(80);
--- misc.js
+++ misc.js
@@ -5,19 +5,19 @@ exports.DecodeHtmlEntity = str => str.replace(/&amp;/g, '&').replace(/&(nbsp|amp
 
 exports.FormatWarnTime = time => {
     let result = '';
-    
+
     const days = Math.trunc(time / 86400000);
     if(days)
         result += `${days} д `;
-    
+
     const hours = Math.trunc((time % 86400000) / 3600000);
     if(hours)
         result += `${hours} ч `;
-        
+
     const minutes = Math.trunc((time % 3600000) / 60000);
     if(minutes)
         result += `${minutes} мин`;
-    
+
     return result;
 };
 
@@ -26,25 +26,25 @@ exports.HttpsGet = url => new Promise(resolve => {
     https.get(url, response => {
         if(response.statusCode != 200)
             return resolve(null);
-        
+
         const chunks = [];
         let dataLen = 0;
-        
+
         response.on('data', data => {
             chunks.push(data);
             dataLen += data.length;
         });
-        
+
         response.on('end', () => {
             const data = Buffer.allocUnsafe(dataLen);
             let len = 0;
-            
+
             for(let i = 0; i < chunks.length; i++) {
                 const chunk = chunks[i];
                 chunk.copy(data, len);
                 len += chunk.length;
             }
-            
+
             return resolve(data);
         });
     });
@@ -54,12 +54,12 @@ exports.GetMentions = str => {
     const
         result = [],
         regExp = /<@!?([0-9]+)>/g;
-    
+
     let match;
     while(match = regExp.exec(str))
         if(match.length > 1)
             result.push(match[1]);
-    
+
     return result;
 };
 
