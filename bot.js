959f70bd2e2f5dae1b95d9e6cb2a4185fe886d47
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2020-06-10 04:05:51 +0500

    Update to discord-slim

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ global.gc && setInterval(global.gc, 3600000);
 const
     Database = require('nedb-promise'),
     MariaDB = require('mariadb'),
-    Discord = require('discordlite'),
+    Discord = require('discord-slim'),
     XmlParser = require('fast-xml-parser'),
     Misc = require('./misc.js'),
     config = require('./config.json');
@@ -622,7 +622,7 @@ client.on('packet', async packet => {
 });
 
 client.Auth(process.env.TOKEN);
-client.Connect(false, Discord.Intents.GUILDS | Discord.Intents.GUILD_MEMBERS | Discord.Intents.GUILD_MESSAGES | Discord.Intents.GUILD_MESSAGE_REACTIONS | Discord.Intents.DIRECT_MESSAGES);
+client.Connect(Discord.Intents.GUILDS | Discord.Intents.GUILD_MEMBERS | Discord.Intents.GUILD_MESSAGES | Discord.Intents.GUILD_MESSAGE_REACTIONS | Discord.Intents.DIRECT_MESSAGES);
 
 if(!(process.env.AUTH_SVC && process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.REDIRECT_URL))
     return;
