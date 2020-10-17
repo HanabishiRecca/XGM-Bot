804fdde763f2632c411c1a378bc633a24b11ec04
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2020-10-18 02:48:47 +0500

    Make logger stringify objects

--- log.js
+++ log.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const CurrentDT = () => new Date().toLocaleString('ru');
+const
+    CurrentDT = () => new Date().toLocaleString('ru'),
+    ToString = value => (typeof value == 'object') ? JSON.stringify(value, null, 4) : value;
 
 const ConsoleLog = console.log;
-console.log = (message, ...params) => ConsoleLog(`[${CurrentDT()}] ${message}`, ...params);
+console.log = (message, ...params) => ConsoleLog(`[${CurrentDT()}] ${ToString(message)}`, ...params);
 
 const ConsoleWarn = console.warn;
-console.warn = (message, ...params) => ConsoleWarn(`\x1b[93m[${CurrentDT()}] ${message}\x1b[0m`, ...params);
+console.warn = (message, ...params) => ConsoleWarn(`\x1b[93m[${CurrentDT()}] ${ToString(message)}\x1b[0m`, ...params);
 
 const ConsoleError = console.error;
-console.error = (message, ...params) => ConsoleError(`\x1b[31m[${CurrentDT()}] ${message}\x1b[0m`, ...params);
+console.error = (message, ...params) => ConsoleError(`\x1b[31m[${CurrentDT()}] ${ToString(message)}\x1b[0m`, ...params);
