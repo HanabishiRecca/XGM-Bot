eabd686a1f2848d844f520fee01bdc0bf124a503
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2020-11-30 22:45:45 +0500

    Logger improvement

--- log.js
+++ log.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const CurrentDT = () => new Date().toLocaleString('ru');
+const CurrentDT = () => `[${new Date().toLocaleString('ru')}]`;
 
 const ConsoleLog = console.log;
-console.log = (...params) => ConsoleLog(`[${CurrentDT()}]`, ...params);
+console.log = (...params) => ConsoleLog('\x1b[0m', CurrentDT(), ...params);
 
 const ConsoleWarn = console.warn;
-console.warn = (...params) => ConsoleWarn('\x1b[93m', `[${CurrentDT()}]`, ...params, '\x1b[0m');
+console.warn = (...params) => ConsoleWarn('\x1b[93m', CurrentDT(), ...params);
 
 const ConsoleError = console.error;
-console.error = (...params) => ConsoleError('\x1b[31m', `[${CurrentDT()}]`, ...params, '\x1b[0m');
+console.error = (...params) => ConsoleError('\x1b[31m', CurrentDT(), ...params);
