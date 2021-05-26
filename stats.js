1e3fa5c7683a8f93b33beddb787b826f346a2dc3
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2021-05-27 01:25:37 +0500

    Split stats into standalone job

---
+++ stats.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import Logger from './log.js';
+
+const Shutdown = (err) => {
+    Logger.Error(err);
+    process.exit(1);
+};
+
+process.on('uncaughtException', Shutdown);
+process.on('unhandledRejection', Shutdown);
+
+Logger.Log('Stats job start.');
+
+if(!(process.env.TOKEN && process.env.MDB_HOST && process.env.MDB_DATABASE && process.env.MDB_USER && process.env.MDB_PASSWORD))
+    Shutdown('No credentials.');
+
+import MariaDB from 'mariadb';
+import { Authorization, Actions, Tools } from 'discord-slim';
+import config from './config.js';
+
+(async () => {
+    const connection = await MariaDB.createConnection({
+        host: process.env.MDB_HOST,
+        database: process.env.MDB_DATABASE,
+        user: process.env.MDB_USER,
+        password: process.env.MDB_PASSWORD,
+        bigNumberStrings: true,
+    });
+
+    const stats = await connection.query('select user,count(id) from messages where (dt >= (CURDATE() - INTERVAL 7 DAY)) group by user order by count(id) desc limit 20;');
+
+    connection.end();
+
+    if(!stats) return;
+
+    let text = '';
+    let index = 1;
+
+    for(const stat of stats)
+        text += `${index++}. ${Tools.Mentions.User(stat.user)} → ${stat['count(id)']}\n`;
+
+    await Actions.Message.Edit(config.stats.channel, config.stats.message, {
+        embed: {
+            title: 'Топ активности за неделю',
+            description: text,
+            timestamp: new Date().toISOString(),
+            color: 16764928,
+        },
+    }, { authorization: new Authorization(process.env.TOKEN) });
+
+    Logger.Log('Stats job finished.');
+})();
