7eab3b8951be0b953ecf8fe5c9c1d6b5681761a3
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2023-05-21 18:40:05 +0000

    Make conditional logging

--- src/util/log.ts
+++ src/util/log.ts
@@ -3,9 +3,18 @@ const DT = () => {
     return `[${str.slice(0, 10)} ${str.slice(11, -1)}]`;
 };
 
-export const
-    Log = (...params: any[]) => console.log('\x1b[0m', DT(), ...params),
-    Warn = (...params: any[]) => console.warn('\x1b[93m', DT(), ...params),
-    Error = (...params: any[]) => console.error('\x1b[31m', DT(), ...params);
+const { CUSTOM_LOG } = process.env;
+
+export const Log = CUSTOM_LOG
+    ? (...params: unknown[]) => console.log("\x1b[0m", DT(), ...params)
+    : console.log;
+
+export const Warn = CUSTOM_LOG
+    ? (...params: unknown[]) => console.warn("\x1b[93m", DT(), ...params)
+    : console.warn;
+
+export const Error = CUSTOM_LOG
+    ? (...params: unknown[]) => console.error("\x1b[31m", DT(), ...params)
+    : console.error;
 
 export default { Log, Warn, Error };
